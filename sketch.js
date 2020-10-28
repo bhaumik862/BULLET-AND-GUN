5180f5fef92252f321146ea6cb63af2a5d89f09a
bhaumik862 <73092909+bhaumik862@users.noreply.github.com>
2020-10-28 16:29:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+var wall,thickness;
+var bullet,speed,weight;
+
+thickness = random(22,83);
+
+function setup() {
+  createCanvas(1600,400);
+
+ wall = createSprite(1200,203,thickness,height/2);
+
+ speed = random(223,321);
+ weight = random(30,52);
+
+}
+
+function draw() {
+  background(255,255,255); 
+
+  drawSprites();
+}
+
+function hasCollided(bullet,wall)
+{
+  bulletRightEdge = bullet.x + bullet.width;
+  wallLeftEdge = wall.x;
+  if(bulletRightEdge>=wallLeftEdge)
+
+{
+  return true;
+}
+return false;
+}
+
+
+if()
